468d84c1d83d5f837d2e3b5f8f497de70416ed4a
mohamed <mohamedhadd95@gmail.com>
2023-04-18 01:18:49 +0100

    my first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const movies = [
+  {
+    title: "The Klouf Mission",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    posterURL: require('./img/0.jpg'),
+    rating: 5,
+    genres: 'Action',
+  },
+  {
+    title: "Janet The Knower",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    posterURL:  require('./img/1.jpg'),
+    rating: 4,
+    genres: 'Action',
+  },
+  {
+    title: "Sliman White",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    posterURL:  require('./img/2.jpg'),
+    rating: 4,
+    genres: 'Horror',
+  },
+  {
+    title: "Seventoui",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    posterURL: require('./img/3.jpg'),
+    rating: 4,
+    genres: 'Comedy',
+  },
+  {
+    title: "Fouchika",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    posterURL: require('./img/4.jpg'),
+    rating: 4,
+    genres: 'Comedy',
+  },
+  
+];
